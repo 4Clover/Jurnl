bad9a19562dc391779ff320369d1e9142331a4e6
anng-y <anngyip59@gmail.com>
2025-05-26 11:09:14 -0700

    routes

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -1,8 +1,28 @@
 <script lang="ts">
     import '../app.css';
-    import Page from './+page.svelte';
 </script>
 
-<main>
-    <Page />
-</main>
+<nav>
+    <ul>
+        <li>
+            <a href="/">Home</a>
+        </li>
+        <li>
+            <a href="/login">Login</a>
+        </li>
+        <li>
+            <a href="/landing">Landing</a>
+        </li>
+        <li>
+            <a href="/journals">Journals</a>
+        </li>
+        <li>
+            <a href="/feed">Friends' Feed</a>
+        </li>
+        <li>
+            <a href="/shared">Shared Journals</a>
+        </li>
+    </ul>
+</nav>
+
+<slot />
