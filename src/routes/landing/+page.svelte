62244cd37ae4e64f012ff2845d8abe17b40ad15a
angelinagyves <gyvesangelina@gmail.com>
2025-05-28 18:32:44 -0700

    Made rudimentary journal preview layout
    includes dummy journals and entries that will later be replaced with backend calls

--- src/routes/landing/+page.svelte
+++ src/routes/landing/+page.svelte
@@ -1,1 +1,72 @@
-<h1>Landing page</h1>
+<!-- Reference: https://www.w3schools.com/howto/howto_css_menu_horizontal_scroll.asp-->
+<script lang="ts">
+
+</script>
+
+<main class = "landing-page">
+    <section class = "user-journals">
+        <h4>My Journals</h4>
+        <div class = "journal-scroll">
+            <div class = "user-journal">
+                <p>Journal #1</p>
+                <div class= journal-preview>
+                    <!-- Photo by Arina Krasnikova: https://www.pexels.com/photo/a-notebooks-and-pen-5712455/-->
+                    <img class = "journal-cover" src= "/src/assets/journal-cover-ex.jpg" alt= "A stack of three journals and a pen on top of a brown blanket.">
+                    <p class = "entry-preview">Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim...</p>
+                </div>
+            </div>
+            <div class = "user-journal">
+                <p>Journal #2</p>
+                <div class= journal-preview>
+                    <!-- Photo by Arina Krasnikova: https://www.pexels.com/photo/a-notebooks-and-pen-5712455/-->
+                    <img class = "journal-cover" src= "/src/assets/journal-cover-ex.jpg" alt= "A stack of three journals and a pen on top of a brown blanket.">
+                    <p class = "entry-preview">Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim...</p>
+                </div>
+            </div>
+            <div class = "user-journal">
+                <p>Journal #3</p>
+                <div class= journal-preview>
+                    <!-- Photo by Arina Krasnikova: https://www.pexels.com/photo/a-notebooks-and-pen-5712455/-->
+                    <img class = "journal-cover" src= "/src/assets/journal-cover-ex.jpg" alt= "A stack of three journals and a pen on top of a brown blanket.">
+                    <p class = "entry-preview">Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim...</p>
+                </div>
+            </div>
+            <div class = "user-journal">
+                <p>Journal #4</p>
+                <div class= journal-preview>
+                    <!-- Photo by Arina Krasnikova: https://www.pexels.com/photo/a-notebooks-and-pen-5712455/-->
+                    <img class = "journal-cover" src= "/src/assets/journal-cover-ex.jpg" alt= "A stack of three journals and a pen on top of a brown blanket.">
+                    <p class = "entry-preview">Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim...</p>
+                </div>
+            </div>
+            <div class = "user-journal">
+                <p>Journal #5</p>
+                <div class= journal-preview>
+                    <!-- Photo by Arina Krasnikova: https://www.pexels.com/photo/a-notebooks-and-pen-5712455/-->
+                    <img class = "journal-cover" src= "/src/assets/journal-cover-ex.jpg" alt= "A stack of three journals and a pen on top of a brown blanket.">
+                    <p class = "entry-preview">Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim...</p>
+                </div>
+            </div>
+            <div class = "user-journal">
+                <p>Journal #6</p>
+                <div class= journal-preview>
+                    <!-- Photo by Arina Krasnikova: https://www.pexels.com/photo/a-notebooks-and-pen-5712455/-->
+                    <img class = "journal-cover" src= "/src/assets/journal-cover-ex.jpg" alt= "A stack of three journals and a pen on top of a brown blanket.">
+                    <p class = "entry-preview">Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim...</p>
+                </div>
+            </div>
+            <div class = "user-journal">
+                <p>Journal #7</p>
+                <div class= journal-preview>
+                    <!-- Photo by Arina Krasnikova: https://www.pexels.com/photo/a-notebooks-and-pen-5712455/-->
+                    <img class = "journal-cover" src= "/src/assets/journal-cover-ex.jpg" alt= "A stack of three journals and a pen on top of a brown blanket.">
+                    <p class = "entry-preview">Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim...</p>
+                </div>
+            </div>
+        </div>
+    </section>
+    <section class = "user-profile">
+    </section>
+    <section class = "public-entries">
+    </section>
+</main>
