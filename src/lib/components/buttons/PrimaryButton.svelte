9b96be7f302e881735d149d17f911a2309c81f89
ethereal-chxn <ethchan@ucdavis.edu>
2025-05-28 23:50:37 -0700

    Creating journal menu component
    - Created primary button component

---
+++ src/lib/components/buttons/PrimaryButton.svelte
@@ -0,0 +1,7 @@
+<script>
+    let {text} = $props()
+</script>
+
+<button class="primary-button">
+    <h3>{text}</h3>
+</button>
---
+++ src/lib/components/journal/JournalMenu.svelte
@@ -0,0 +1,10 @@
+<script>
+    import PrimaryButton from "../buttons/PrimaryButton.svelte";
+</script>
+
+<div class="journal-menu">
+    <PrimaryButton text={2025}/>
+    <div>
+
+    </div>
+</div>
