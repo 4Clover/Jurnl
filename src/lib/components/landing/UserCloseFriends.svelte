c0549200deecc1f54c7b60baa5b89dba9d1a77b0
angelinagyves <gyvesangelina@gmail.com>
2025-06-02 18:35:12 -0700

    Added props for close friends

--- src/lib/components/landing/UserCloseFriends.svelte
+++ src/lib/components/landing/UserCloseFriends.svelte
@@ -1,9 +1,20 @@
 <script lang="ts">
     import type { UserFriendsProps } from "$lib/types/landing.types";
+
+    let { closeFriendsList } : UserFriendsProps = $props();
 </script>
 
 <h2>My Close Friends</h2>
 <div class="manage-friends">
+    <!--
+    {#each closeFriendsList as closeFriend}
+        <div class="manage-friend">
+            <p>{closeFriend}</p>
+            <button class="delete-friend-button">DELETE</button>
+        </div>
+    {/each}
+    -->
+    
     <div class="manage-friend">
         <p>Friend 1</p>
         <button class="delete-friend-button">DELETE</button>
