60a56d304f80cf34c47c5c507de8f2bd263dc828
angelinagyves <gyvesangelina@gmail.com>
2025-06-02 18:08:18 -0700

    Changed journal covers to be setable colors instead of images

--- src/lib/components/landing/UserJournals.svelte
+++ src/lib/components/landing/UserJournals.svelte
@@ -10,7 +10,7 @@
     {#each journalList as journal}
         <UserJournal 
             journalTitle={journal.title}
-            journalCoverUrl={journal.cover_color}
+            journalColor={journal.cover_color}
             latestJournalEntries={/*not sure how this will be done yet*/}
         />
     {/each}
@@ -19,8 +19,7 @@
     <div class = "user-journal">
         <p>Journal #1</p>
         <div class= journal-preview>
-            <!-- Photo by Arina Krasnikova: https://www.pexels.com/photo/a-notebooks-and-pen-5712455/-->
-            <img class = "journal-cover" src= "/src/assets/journal-cover-ex.jpg" alt= "A stack of three journals and a pen on top of a brown blanket.">
+            <div class = "journal-cover" style = "background-color:#a2aec6;"></div>
             <div class = "entry-previews">
                 <button class = "entry-preview-button" >Entry 1</button>
                 <button class = "entry-preview-button" >Entry 2</button>
@@ -33,8 +32,7 @@
     <div class = "user-journal">
         <p>Journal #2</p>
         <div class= journal-preview>
-            <!-- Photo by Arina Krasnikova: https://www.pexels.com/photo/a-notebooks-and-pen-5712455/-->
-            <img class = "journal-cover" src= "/src/assets/journal-cover-ex.jpg" alt= "A stack of three journals and a pen on top of a brown blanket.">
+            <div class = "journal-cover" style = "background-color:#a2aec6;"></div>
             <div class = "entry-previews">
                 <button class = "entry-preview-button" >Entry 1</button>
                 <button class = "entry-preview-button" >Entry 2</button>
@@ -47,8 +45,7 @@
     <div class = "user-journal">
         <p>Journal #3</p>
         <div class= journal-preview>
-            <!-- Photo by Arina Krasnikova: https://www.pexels.com/photo/a-notebooks-and-pen-5712455/-->
-            <img class = "journal-cover" src= "/src/assets/journal-cover-ex.jpg" alt= "A stack of three journals and a pen on top of a brown blanket.">
+            <div class = "journal-cover" style = "background-color:#a2aec6;"></div>
             <div class = "entry-previews">
                 <button class = "entry-preview-button" >Entry 1</button>
                 <button class = "entry-preview-button" >Entry 2</button>
@@ -61,8 +58,7 @@
     <div class = "user-journal">
         <p>Journal #4</p>
         <div class= journal-preview>
-            <!-- Photo by Arina Krasnikova: https://www.pexels.com/photo/a-notebooks-and-pen-5712455/-->
-            <img class = "journal-cover" src= "/src/assets/journal-cover-ex.jpg" alt= "A stack of three journals and a pen on top of a brown blanket.">
+            <div class = "journal-cover" style = "background-color:#a2aec6;"></div>
             <div class = "entry-previews">
                 <button class = "entry-preview-button" >Entry 1</button>
                 <button class = "entry-preview-button" >Entry 2</button>
@@ -75,8 +71,7 @@
     <div class = "user-journal">
         <p>Journal #5</p>
         <div class= journal-preview>
-            <!-- Photo by Arina Krasnikova: https://www.pexels.com/photo/a-notebooks-and-pen-5712455/-->
-            <img class = "journal-cover" src= "/src/assets/journal-cover-ex.jpg" alt= "A stack of three journals and a pen on top of a brown blanket.">
+            <div class = "journal-cover" style = "background-color:#a2aec6;"></div>
             <div class = "entry-previews">
                 <button class = "entry-preview-button" >Entry 1</button>
                 <button class = "entry-preview-button" >Entry 2</button>
