1042829b80e08a04b9013ea25fef0674d61d6f41
4Clover <dlmannion@ucdavis.edu>
2025-06-02 18:32:22 -0700

    Config update for dev server load speeds and database storage

--- svelte.config.js
+++ svelte.config.js
@@ -1,23 +1,55 @@
 import adapter from '@sveltejs/adapter-auto';
 import { vitePreprocess } from '@sveltejs/vite-plugin-svelte';
+// TODO: SITE CONFIG WEBSITES
 /** @type {import('@sveltejs/kit').Config} */
-
-const config = {
+export default {
+    // Preprocessor configuration
     preprocess: vitePreprocess(),
-    kit: { adapter: adapter() },
-    alias: {
-        lib: './src/lib',
-        routes: './src/routes',
-        types: './src/lib/types',
-        auth: './src/lib/server/auth',
-        components: './src/lib/components',
-        server: './src/lib/server',
-        database: './src/lib/server/database',
-        schemas: './src/lib/server/database/schemas',
+
+    // SvelteKit configuration
+    kit: {
+        adapter: adapter(),
+
+        env: {
+            dir: '.',
+            publicPrefix: 'PUBLIC_',
+        },
+
+        csp: {
+            mode: 'auto',
+            directives: {
+                'script-src': ['self'],
+            },
+        },
+
+        files: {
+            assets: 'static',
+            lib: 'src/lib',
+            routes: 'src/routes',
+            serviceWorker: 'src/service-worker',
+            appTemplate: 'src/app.html',
+            errorTemplate: 'src/error.html',
+        },
+
+        alias: {
+            $lib: 'src/lib',
+            $routes: 'src/routes',
+            $types: 'src/lib/types',
+            $auth: 'src/lib/server/auth',
+            $components: 'src/lib/components',
+            $server: 'src/lib/server',
+            $database: 'src/lib/server/database',
+            $schemas: 'src/lib/server/database/schemas',
+            $styles: 'src/styles',
+            $assets: 'src/assets',
+        },
     },
+
+    // Compiler options
     compilerOptions: {
         runes: true,
+        hydratable: true,
+        enableSourcemap: true,
     },
 };
 
-export default config;
