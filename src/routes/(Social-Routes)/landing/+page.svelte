c49399574efcc8d396d30f96f6a26ab4340925e1
ethereal-chxn <ethchan@ucdavis.edu>
2025-06-10 14:41:08 -0700

    User profile setup now working

--- src/routes/(Social-Routes)/landing/+page.svelte
+++ src/routes/(Social-Routes)/landing/+page.svelte
@@ -6,19 +6,25 @@
     import UserPublicEntries from '$components/landing/UserPublicEntries.svelte';
     import type { PageProps } from './$types';
     let { data }: PageProps = $props();
-    
+
     console.log(data);
 </script>
 
 <main class="landing-page">
     <section class="user-journals">
-        <UserJournals journalList={data.user.journals}/>
+        <UserJournals journalList={data.user.journals} />
     </section>
     <section class="user-profile">
-        <UserProfile userInfo={{ username: data.user.username_display }} />
+        <UserProfile
+            userInfo={{
+                username: data.user.username_display,
+                bio_image_url: data.user.bio_image_url,
+                bio_text: data.user.bio_text,
+            }}
+        />
     </section>
     <section class="user-close-friends">
-        <UserCloseFriends userInfo={data.user}/>
+        <UserCloseFriends userInfo={data.user} />
     </section>
     <section class="user-public-entries">
         <UserPublicEntries />
