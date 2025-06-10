c50eddc7f362b6b184312795bd898f18275f9134
ethereal-chxn <ethchan@ucdavis.edu>
2025-06-09 23:09:57 -0700

    Started work on user profile setup page

--- src/routes/(Social-Routes)/profile/+page.svelte
+++ src/routes/(Social-Routes)/profile/+page.svelte
@@ -1,1 +1,9 @@
-﻿<h1> USER PROFILE SETUP </h1>
+﻿<script>
+    import PrimaryButton from '$lib/components/buttons/PrimaryButton.svelte';
+    import UserProfileForm from '$lib/components/forms/UserProfileForm.svelte';
+</script>
+
+<h1>USER PROFILE SETUP</h1>
+
+<UserProfileForm />
+<PrimaryButton text="Submit" />
