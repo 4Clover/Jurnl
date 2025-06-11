c7a245378359d8c03da57678e98ac8938937cb2e
Dillon Mannion <dillonmannion16@gmail.com>
2025-06-11 10:06:43 -0700

    test implemented

--- src/test/Friends.svelte.test.ts
+++ src/test/Friends.svelte.test.ts
@@ -1,4 +1,4 @@
-import { test, expect, it, describe } from 'vitest';
+import { expect, it, describe } from 'vitest';
 import { render } from '@testing-library/svelte';
 import FeedItem from '$lib/components/friends/FeedItem.svelte';
 
@@ -7,12 +7,25 @@ describe('FeedItem', () => {
         const friend = {
             username: 'Friend',
             imgurl: 'flower.png',
-            entries: ['First Entry', 'Second Entry'],
+            entries: [
+                {
+                    _id: 'entry1',
+                    title: 'First Entry',
+                    journal: 'journal1'
+                },
+                {
+                    _id: 'entry2', 
+                    title: 'Second Entry',
+                    journal: 'journal1'
+                }
+            ],
         };
 
         const { getByText } = render(FeedItem, { friend });
 
-        expect(getByText('Friend')).toBeInTheDocument();
-        expect(getByText('First Entry')).toBeInTheDocument();
+        expect(getByText('Friend')).toBeTruthy();
+        expect(getByText('First Entry')).toBeTruthy();
+        expect(getByText('Second Entry')).toBeTruthy();
+        expect(getByText('Visit')).toBeTruthy();
     });
 });
