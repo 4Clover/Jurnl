279c62e0ca700e94abc560fbd7b814bfce1494d3
ethereal-chxn <ethchan@ucdavis.edu>
2025-06-10 20:18:39 -0700

    Nav bar tweaks
    - added link buttons to journals page and profile edit page

--- src/lib/components/nav/NormalNav.svelte
+++ src/lib/components/nav/NormalNav.svelte
@@ -1,19 +1,16 @@
-﻿<script lang="ts" >
+﻿<script lang="ts">
     import LogInOutButton from '$lib/components/buttons/LogInOutButton.svelte';
 </script>
 
-
-<button class="pajamas--hamburger" aria-label="Menu Button"></button>
+<!-- <button class="pajamas--hamburger" aria-label="Menu Button"></button> -->
 <button onclick={() => (window.location.href = '/landing')}>
-    <h1 class="navbar__logo">
-        Jurnl
-    </h1>
-</button> 
-
+    <h1 class="navbar__logo">Jurnl</h1>
+</button>
 
 <div class="navbar__links" id="navbar-links">
     <a href="/landing">Landing</a>
     <a href="/feed">Friends' Feed</a>
+    <a href="/journals">My Journals</a>
+    <a href="/profile">My Profile</a>
 </div>
 <LogInOutButton />
-
